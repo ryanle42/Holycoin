80e65203aa67e5bcf1ad01848f5a425962c60060
ryan <rleinbox@gmail.com>
2017-07-29 13:12:16 -0700

    updated test file

--- test/test.js
+++ test/test.js
@@ -1,16 +1,30 @@
 var Crowdsale = artifacts.require("./Crowdsale.sol")
 describe("Crowdsale", () => {
 	contract("transfer", (accounts) => {
-		it("should transfer 200", (done) => {
-			 (async () => {
-				await Crowdsale.deployed()
-				let instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+		
+		it("should transfer 10000", () => {
+			return Promise.resolve()
+			.then(() => Crowdsale.deployed())
+			.then(() => {
+				instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
 				web3.eth.sendTransaction({from: accounts[0], to: instance.address, value: 10000})
+			})
+		})
+		it("transfer 10" , () => {
+			return Promise.resolve()
+			.then(() => {
+				instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+				web3.eth.sendTransaction({from: accounts[0], to: instance.address, value: 10})
+			})
+		})
+		it("print accounts", () => {
+			return Promise.resolve()
+			.then(() => {
 				for (let i = accounts.length - 1; i >= 0; i--) {
-				console.log("amount in acc", i, instance.balanceOf(accounts[i]))
+					instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+					console.log("amount in acc", i, instance.balanceOf(accounts[i]))
 				}
-				
-			})().then(() => done(), done);
+			})
 		})
 	})
 })
