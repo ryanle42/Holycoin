94b04b26b1b84f4a6df36ac626d17893bc4b3417
Tim Jose <tjose@tims-mbp.gateway.sonic.net>
2017-07-29 13:23:58 -0700

    holycoin test

---
+++ test/test_holycoin.js
@@ -0,0 +1,29 @@
+var Crowdsale = artifacts.require("./Crowdsale.sol")
+describe("Crowdsale", () => {
+	contract("transfer", (accounts) => {
+		it("should transfer 10000", () => {
+			return Promise.resolve()
+			.then(() => Crowdsale.deployed())
+			.then(() => {
+				instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+				web3.eth.sendTransaction({from: accounts[0], to: instance.address, value: 10000})
+			})
+		})
+		it("transfer -10" , () => {
+			return Promise.resolve()
+			.then(() => {
+				instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+				web3.eth.sendTransaction({from: accounts[0], to: instance.address, value: -10})
+			})
+		})
+		it("print accounts", () => {
+			return Promise.resolve()
+			.then(() => {
+				for (let i = accounts.length - 1; i >= 0; i--) {
+					instance = web3.eth.contract(Crowdsale.abi).at(Crowdsale.address)
+					console.log("amount in acc", i, instance.balanceOf(accounts[i]))
+				}
+			})
+		})
+	})
+})
