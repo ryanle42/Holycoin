1bf3fe38196f1154a3d549ca4a947bb1ca4f4e69
ryan <rleinbox@gmail.com>
2017-07-29 17:50:13 -0700

    website

---
+++ src/confess/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link type="text/css" rel="stylesheet" href="../css/style.css"/>
+	<title>Confession Page</title>
+</head>
+<body>
+	<h1><a href="../" class="home">HolyCoin</a></h1>
+	<div class="header">
+		<a class="indulgences" href="../indulgences">Indulgences</a>
+		<a class="confess" href="./">Confess</a>
+	</div>
+	<h2>Confess your sins</h2>
+	<form>
+		<input type="text" id="confession-box">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
---
+++ src/indulgences/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link type="text/css" rel="stylesheet" href="../css/style.css"/>
+	<title>Indulgences Page</title>
+</head>
+<body>
+	<h1><a href="../" class="home">HolyCoin</a></h1>
+	<div class="header">
+		<a class="indulgences" href="./">Indulgences</a>
+		<a class="confess" href="../confess">Confess</a>
+	</div>
+	<h2>Pay your indulgence</h2>
+	<form>
+		<input type="number">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
